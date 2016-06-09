5cf82282f5c9bef384dbda776feb0a00b65631cc
Oliver Wehrens <oliver@wehrens.de>
2016-06-09 15:49:02 +0200

    Show the numbers of the found artifacts in the overview.

--- src/main/resources/static/js/overview.js
+++ src/main/resources/static/js/overview.js
@@ -1,14 +1,20 @@
 $(function () {
     $('#searchbox').keyup(function () {
         var searchTerm = $(this).val().toLowerCase();
-
-        console.log(searchTerm);
-
         $(`.card[data-meta*='${searchTerm}']`).show();
         $('.card').not(`[data-meta*='${searchTerm}']`).hide();
 
         if (searchTerm.trim() == '') {
             $(".card").show();
         }
-    })
+        countArtifacts();
+    });
+
+    countArtifacts();
 });
+
+
+function countArtifacts() {
+    var n = $('a.ui.card:not([style*="display: none"])').length;
+    $("#artifactsheader").text("Matching Artifacts (" + n + ")");
+};

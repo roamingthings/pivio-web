96ffcde0683f5b259cf651d6b21e9d4a478e43b9
Oliver Wehrens <oliver@wehrens.de>
2016-08-17 08:44:16 +0200

    Sorting by name.

--- src/main/java/io/pivio/view/PivioServerConnector.java
+++ src/main/java/io/pivio/view/PivioServerConnector.java
@@ -43,7 +43,7 @@ public class PivioServerConnector {
     }
 
     public List<Overview> getOverviews() throws IOException {
-        String path = "/document?fields=short_name,id,description,name,owner,context,lastUpdate,lastUpload&sort=lastUpdate:desc";
+        String path = "/document?fields=short_name,id,description,name,owner,context,lastUpdate,lastUpload&sort=name:asc";
         String url = serverConfig.apiAddress + path;
         RestTemplate restTemplate = new RestTemplate();
         ParameterizedTypeReference<List<Overview>> typeRef = new ParameterizedTypeReference<List<Overview>>() {
